8d843b3324b3e53f92dfc4a03b794898c93742d6
LeonardoCalder <leoside0808@gmail.com>
2025-04-22 23:01:14 -0500

    Update app.js

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ document.getElementById('form').addEventListener('submit', function(event) {
   console.log(serviceRequestData);
 
   // Enviar la solicitud al backend
-  fetch('historia-clinica-2.onrender.com/service-request/', {
+  fetch('https://historia-clinica-2.onrender.com/service-request/', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(serviceRequestData)
